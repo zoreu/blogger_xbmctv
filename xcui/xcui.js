ac60da04c5d37fdfae5089d1615b6c2d6185ac6c
Joel Silva <53149347+zoreu@users.noreply.github.com>
2025-11-14 01:29:12 -0300

    Update xcui.js

--- xcui/xcui.js
+++ xcui/xcui.js
@@ -1,3 +1,8 @@
+if (window.location.protocol === "https:") {
+    let novaURL = "http://" + window.location.host + window.location.pathname + window.location.search;
+    window.location.replace(novaURL);
+}
+
 // Variáveis globais
 let currentSection = 'live';
 let credentials = {};
